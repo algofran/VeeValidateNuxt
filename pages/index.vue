8f511d17d9cc289a8e5e56411fd82773405b37af
Abdul Gaffar <algofran@gmail.com>
2022-07-19 22:06:00 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h3>VeeValidate - Nuxt Example:</h3>
+    <ValidationProvider rules="required|alpha" v-slot="{ errors }">
+      <input type="text" v-model="value">
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>
+  </section>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+
+export default {
+  components: {
+    ValidationProvider
+  },
+  data: () => ({
+    value: ""
+  })
+};
+</script>
